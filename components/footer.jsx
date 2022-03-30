a19f1b68de6b70284421e4abef5537c1ebaa96a4
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-30 12:57:23 +0800

    update about us

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ export default function FooterBtm() {
 
     return   <footer className="border-t flex justify-between items-center h-20">
         <div className="flex justify-between items-center 2xl:w-1536 m-auto px-10 w-full ">
-            <a className="text-gray-500 text-lg font-cal hover:text-black transition-all ease duration-150" href={`/${obj.blog}`}>Blog</a>
+            <a className="text-gray-500 text-lg font-cal hover:text-black transition-all ease duration-150" href={`/${obj.Blog}`}>Blog</a>
             <div className="flex space-x-8">
                 <a href={`https://discord.com/invite/${obj.Discord}`} target="_blank" rel="noreferrer">
                     <img src="/assets/images/discord.svg" alt="" className="text-gray-500 hover:text-black transition-all ease duration-150"/>
