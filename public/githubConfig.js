abc8089796dc9da49be75167c94467a63870c8bb
wamma9527 <117485655@qq.com>
2022-03-30 12:20:32 +0800

    Update githubConfig.js

--- public/githubConfig.js
+++ public/githubConfig.js
@@ -1,7 +1,7 @@
 const clientID = 'Iv1.94a72acba60a8147';
 const authorizeUri = 'https://github.com/login/oauth/authorize';
-const redirectUri = 'https://www.daopark.xyz';
-const baseUrl = 'https://www.daopark.xyz';
+const redirectUri = 'https://daopark.xyz';
+const baseUrl = 'https://daopark.xyz';
 const backEnd = 'https://dp-api.daopark.xyz';
 
 
