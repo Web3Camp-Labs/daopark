ba84c549ca1a66d08a8b195e886c538121f8336e
xrdavies <xrdavies@users.noreply.github.com>
2022-03-31 17:38:51 +0000

    deploy: 84fdcd3b2d21649ed9f5e31279185c3137ce5aab

---
+++ githubConfig.js
@@ -0,0 +1,18 @@
+const clientID = 'Iv1.94a72acba60a8147';
+const authorizeUri = 'https://github.com/login/oauth/authorize';
+const redirectUri = 'https://daopark.xyz';
+const baseUrl = 'https://daopark.xyz';
+const backEnd = 'https://dp-api.daopark.xyz';
+const org = "Web3-Camp"
+const repo = "test-issue";
+
+
+export default {
+    clientID,
+    authorizeUri,
+    redirectUri,
+    baseUrl,
+    backEnd,
+    org,
+    repo
+}
