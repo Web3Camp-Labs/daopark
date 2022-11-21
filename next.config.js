12c0dbedb246d5c5c591918c5b4d76372601580c
throwaway <7518647+wendychaung@users.noreply.github.com>
2022-11-21 20:03:57 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   trailingSlash: true,
   experimental: {
     // ssr and displayName are configured by default
     styledComponents: true,
 
   },
+  assetPrefix: isProd ?'/':'',
   webpack: (config, { webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin({
       resourceRegExp: /^electron$/
